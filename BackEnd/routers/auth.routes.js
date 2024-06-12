a70fd3f9a9a3b640b5f8f1ca2898357327c1a9ac
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2024-06-12 10:14:45 +0530

    small rename

--- BackEnd/routers/auth.routes.js
+++ BackEnd/routers/auth.routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router();
 const validateDto = require('../middleware/validate-dto.service')
 
-const userSchema = require('../schema/user.schema')
+const registerSchema = require('../schema/register.schema')
 const loginSchema = require('../schema/login.schema')
 
 const{test,registerUser,loginUser} = require("../controllers/auth.controller");
 
 router.get('/',test)
-router.post("/register",validateDto(userSchema),registerUser)
+router.post("/register",validateDto(registerSchema),registerUser)
 router.post("/login",validateDto(loginSchema),loginUser)
 
 module.exports = router
